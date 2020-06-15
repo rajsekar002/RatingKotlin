5c6ee8575dbae7f4fc5fd4ae14975dc14740eee0
pavan kvch <pavankvch@gmail.com>
2020-06-15 09:40:54 +0530

    Initial commit

---
+++ app/src/main/java/com/androindian/ratingkotlin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.androindian.ratingkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ratingBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
+            Toast.makeText(this@MainActivity,""+rating,Toast.LENGTH_LONG).show()
+        }
+    }
+}
